c149d4ca0f869e99447d5eac9bccc67032b855ac
menzzi <minmin3927@naver.com>
2024-11-06 17:38:06 +0900

    test : StockRepositoryTest 코드 수정

--- src/test/java/store/model/StockRepositoryTest.java
+++ src/test/java/store/model/StockRepositoryTest.java
@@ -23,14 +23,14 @@ public class StockRepositoryTest {
 
         List<Stock> stocks = stockRepository.getAllStocks();
         assertEquals(16, stocks.size());
-        assertEquals("콜라", stocks.get(0).getName());
-        assertEquals(1000, stocks.get(0).getPrice());
-        assertEquals(10, stocks.get(0).getQuantity());
-        assertEquals("탄산2+1", stocks.get(0).getPromotion());
+        assertEquals("콜라", stocks.getFirst().getName());
+        assertEquals(1000, stocks.getFirst().getPrice());
+        assertEquals(10, stocks.getFirst().getQuantity());
+        assertEquals("탄산2+1", stocks.getFirst().getPromotion());
 
-        assertEquals("콜라", stocks.get(1).getName());
-        assertEquals(1000, stocks.get(1).getPrice());
-        assertEquals(10, stocks.get(1).getQuantity());
-        assertEquals("null", stocks.get(1).getPromotion());
+        assertEquals("컵라면", stocks.getLast().getName());
+        assertEquals(1700, stocks.getLast().getPrice());
+        assertEquals(10, stocks.getLast().getQuantity());
+        assertEquals("null", stocks.getLast().getPromotion());
     }
 }
