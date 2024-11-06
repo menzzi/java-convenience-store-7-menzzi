a1efb822095e3948480edacec1ac75c2f72f2f45
menzzi <minmin3927@naver.com>
2024-11-06 15:54:04 +0900

    feat(model - domain) : 출력형식에 맞추어 toString 메서드 수정, 추가 메서드 구현

--- src/main/java/store/model/domain/Stock.java
+++ src/main/java/store/model/domain/Stock.java
@@ -37,7 +37,28 @@ public class Stock {
 
     @Override
     public String toString() {
+        StringBuilder sb = new StringBuilder(makeCommonString());
+        if(quantity == 0){
+            return outOfStockString(sb);
+        }
+        sb.append(quantity).append("개 ");
+        if(promotion.equals("null")){
+            return sb.toString();
+        }
+        sb.append(promotion);
+        return sb.toString();
+    }
+
+    private String makeCommonString(){
         DecimalFormat df = new DecimalFormat("###,###");
-        return "- " + name + " " + df.format(price) + "원 " +  quantity + "개 " + promotion;
+        return "- " + name + " " + df.format(price) + "원 ";
+    }
+
+    private String outOfStockString(StringBuilder sb){
+        sb.append("재고없음 ");
+        if(promotion.equals("null")) {
+            return sb.toString();
+        }
+        return sb.append(promotion).toString();
     }
 }
