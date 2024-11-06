35f3c1dd02e4715e7a39752e2ed45647339d3606
menzzi <minmin3927@naver.com>
2024-11-06 14:59:19 +0900

    feat(view) : OutputView 구현

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,13 @@
+package store.view;
+
+public class OutputView {
+    public void printWelcomeGreeting(){
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.");
+        System.out.println();
+    }
+
+    public void printStockInformation(String stockDetails){
+        System.out.println(stockDetails);
+    }
+}
