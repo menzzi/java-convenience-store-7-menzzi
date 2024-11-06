472bbeff80992d1521b38c4c174cc0f9e31f5dc2
menzzi <minmin3927@naver.com>
2024-11-06 13:41:17 +0900

    feat(model) : stock 클래스 구현 (재고저장)

---
+++ src/main/java/store/model/stock.java
@@ -0,0 +1,40 @@
+package store.model;
+
+public class stock {
+    private final String name;
+    private final int price;
+    private int quantity;
+    private final String promotion;
+
+    public stock(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{name='" + name + "', price=" + price + ", quantity=" + quantity + ", promotion='" + promotion + "'}";
+    }
+}
