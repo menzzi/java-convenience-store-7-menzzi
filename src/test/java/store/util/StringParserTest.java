96591dc9c2788b5568fe21f770dbd17bae9dd0c1
menzzi <minmin3927@naver.com>
2024-11-10 17:03:38 +0900

    test : StringParserTest 파마리터 테스트 적용

--- src/test/java/store/util/StringParserTest.java
+++ src/test/java/store/util/StringParserTest.java
@@ -5,30 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class StringParserTest {
 
-    @Test
-    void 대괄호_형식을_지키지_않으면_예외가_발생한다() {
-        String input = "콜라-2,사이다-3";
-        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-            StringParser.validateOrderFormat(input);
-        });
-        assertEquals(" 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.", thrown.getMessage());
-    }
-
-    @Test
-    void 대괄호_형식을_지키지_않으면_예외가_발생한다2() {
-        String input = "(콜라-2),(사이다-3)";
-        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-            StringParser.validateOrderFormat(input);
-        });
-        assertEquals(" 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.", thrown.getMessage());
-    }
-
-    @Test
-    void 대괄호_형식을_지키지_않으면_예외가_발생한다3() {
-        String input = "[콜라-2,사이다-3]";
+    @ParameterizedTest
+    @CsvSource({
+            "콜라-2,사이다-3",
+            "(콜라-2),(사이다-3)",
+            "[콜라-2,사이다-3]"
+    })
+    void 대괄호_형식을_지키지_않으면_예외가_발생한다(String userInput) {
+        String input = userInput;
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
             StringParser.validateOrderFormat(input);
         });
