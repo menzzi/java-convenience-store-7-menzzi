0782e683b87d3e763407134de595adb160759dfc
menzzi <minmin3927@naver.com>
2024-11-11 16:27:04 +0900

    refactor(repository) : getAllStocks 메서드 static 제거

--- src/main/java/store/model/repository/StockRepository.java
+++ src/main/java/store/model/repository/StockRepository.java
@@ -58,7 +58,7 @@ public class StockRepository {
         stocks.add(new Stock(name, price, quantity, promotion));
     }
 
-    public static List<Stock> getAllStocks() {
+    public List<Stock> getAllStocks() {
         return stocks;
     }
 }
